279546a5547b9995c11396a8bbddde3e4a2cdb62
Grapaxels <nikhilkumar1338@gmail.com>
2024-05-04 17:52:29 +0530

    add: pages

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import login from '../assets/login.png'
+import logo from '../assets/logo.png'
+import menu from '../assets/menu.png'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <>
-    <img src={login} alt='login'/>
+    
+    <header className='header'>
+        <img className='logo' src={logo} alt='logo'/>
+        <h1 className='we'>We<span className='call'>Call</span></h1>
+        <Link to='/login'><img src={login} className='login' alt='login'/></Link>
+        <Link to={'/Menu'}><img className='menu' src={menu} alt='menu'/></Link>              
+
+    </header>
     </>
   )
 }
