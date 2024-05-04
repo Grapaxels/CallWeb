b189fa8f9e0e68b05bdc1e54b15d8a8204624b89
Grapaxels <nikhilkumar1338@gmail.com>
2024-05-04 17:31:35 +0530

    add: pages

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <>
-    <img src='src\assets\login.png' alt='login'></img>
+    <img src='src/assets/login.png' alt='login'></img>
     </>
   )
 }
